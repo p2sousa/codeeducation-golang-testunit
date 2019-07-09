64f78bdb25880622904ff1704d455648b35ab822
Pablo Sousa <pablosousa.ads@gmail.com>
2019-07-08 23:19:01 -0300

    adicionado arquivos .go

--- src/app.go
+++ src/app.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func sum(number1 int, numebr2 int) int {
+func Sum(number1 int, numebr2 int) int {
 	return number1 + numebr2;
 }
 
 func main() {
-	res := sum(5, 5)
+	res := Sum(5, 5)
     fmt.Println("5+5 =", res)
 }
